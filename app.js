75647ee3bca44865f20b035f66e6bf20ef71ae73
apstemmer <apstemmer@hotmail.com>
2016-07-20 12:31:04 +0200

    added jsmodbus and queue system

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 "use strict"
-const dgram = require('dgram');
-const modbus = require('modbus-stack');
+var fp = require('./modbus.js');
+
+fp.addCommand('light','off');
+
+
+
 
 function init() {
-	var sock = dgram.createSocket('udp4');
-	sock.bind(35353);
-
-	sock.on('message',(msg, rinfo) => {
-		console.log('got the message');
-	});
-
-	sock.on('listening',() => {
-		Homey.log('listening to UDP');
-	});
+	
 }
 
 module.exports.init = init;
--- modbus.js
+++ modbus.js
@@ -1,19 +1,128 @@
-"use strict"
+'use strict'
+var modbus = require('jsmodbus');
 
-// 'RIR' contains the "Function Code" that we are going to invoke on the remote device
-var RIR = require('modbus-stack').FUNCTION_CODES.READ_INPUT_REGISTERS;
 
-// IP and port of the MODBUS slave, default port is 502
-var client = require('modbus-stack/client').createClient(502, '10.0.1.50');
+var queue = [];
 
-// 'req' is an instance of the low-level `ModbusRequestStack` class
-var req = client.request(RIR, // Function Code: 4
-                         0,    // Start at address 0
-                         50);  // Read 50 contiguous registers from 0
+const 	MODBUS_PORT = 502,//standard port for MODBUS
+		FIREPLACE_STATUS_REG = 40203,
+		FIREPLACE_ACTION_REG = 40200,
+		ROOM_TEMPERATURE_REG = 40207;
 
-// 'response' is emitted after the entire contents of the response has been received.
-req.on('response', function(registers) {
-  // An Array of length 50 filled with Numbers of the current registers.
-  console.log(registers);
-  client.end();
-});
+var unitID = 2;//default initial value
+var ip = '';
+
+var funcs = {
+	'light':light
+}
+var currentCommand;
+//Potentially use callback for error handling
+function connect(callback){
+	//Get IP address using mDNS
+	console.log('connecting...')
+	ip = '192.168.0.51';
+	var client = modbus.client.tcp.complete({
+		'host'				: ip,
+		'port'				: MODBUS_PORT,
+		'autoReconnect'		: true,
+		'reconnectTimeout'	: 1000,
+		'timeout'			: 5000,
+		'unitId'			: unitID
+	});
+	client.connect();
+
+	client.once('connect',function(){//once
+		console.log(`connected to port ${client.port} on ${client.host}, using unitID ${client.unitId}`);
+		return callback(null,client);
+	});
+
+	client.on('error',function(err){
+		console.log(err);
+		return callback(err,null);
+	});
+}
+
+function light(mode, callback){
+	var callback = callback || function(){}
+	var register = undefined, check = undefined;
+
+	if(mode == 'on'){
+		register = 103;
+		check = 0;
+	}else if(mode == 'off'){
+		register = 5;
+		check = 256;
+	}else{
+		//throw error
+	}
+
+	if(this.getState() == 'connect'){
+		this.readHoldingRegisters(FIREPLACE_STATUS_REG,1).then((resp) => {
+			
+			if((resp.register[0]&256) == check){//lights are off/on
+				this.writeSingleRegister(FIREPLACE_ACTION_REG,register).then(function(offresp){
+					console.log(`Turned lights ${mode}`);
+					callback(null);
+				});
+			}else{
+				console.log(`Lights are already ${mode}`);
+			}
+
+		}).fail(function(err){
+
+			console.log(err);
+
+		});
+
+	}
+}
+
+
+function addCommand(cmd, args, callback){
+	var callback = callback || function(){}
+	
+	queue.push({
+		'command':cmd,
+		'args':args,
+		'callback':callback
+	});
+
+	nextCommand();
+}
+
+function nextCommand(){
+	
+	if(typeof currentCommand !== 'undefined') {
+		console.log('No task defined');
+		return
+	};
+	if( queue.length < 1 ){
+		console.log('No more tasks');
+		return
+	};
+
+	
+	currentCommand = queue.shift()
+	
+	connect((e,client) => {
+
+		if(e){
+			console.log(e);
+			return
+		}
+		
+		//potentially turn into apply if more arguments are required
+		funcs[currentCommand.command].call(client, currentCommand.args,(err, result) => {
+			console.log(currentCommand.callback);
+			
+			nextCommand();
+			client.close();
+		});
+
+	});
+
+}
+
+module.exports = {
+	'addCommand': addCommand
+}
